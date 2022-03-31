e7bbff75e87de5a55800c563f9a08ae80547657a
A1L9 <al19081987@gmail.com>
2022-03-31 19:29:20 -0300

    Initial commit

--- vis_data.R
+++ vis_data.R
@@ -1,5 +1,5 @@
 # Title:    visualização de Dados
-# Author:   Antonio Sergio da Silva
+# Author:   Aline Lopes da Silva
 # File:     Fundamentos da Linguagem R
 # Project:  League Online Courses SGQ
 # Purpose:
